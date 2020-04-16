c1630367ae2c14fa43e97af850aeafdc235b632a
Anne-Kathrin <annekdrotleff@gmail.com>
2020-04-16 14:36:15 +0200

    Revert "initial commit"
    This reverts commit 8cf200f3da8dc4ff0ee056dc3888202c92ee489c.

--- stencil/my-component/dist/esm/loader.mjs
+++
@@ -1,7 +0,0 @@
-import { a as patchEsm, b as bootstrapLazy } from './index-c8af5f01.js';
-
-const defineCustomElements = (win, options) => patchEsm().then(() => {
-  return bootstrapLazy([["my-component",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]]], options);
-});
-
-export { defineCustomElements };
