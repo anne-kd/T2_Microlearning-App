6b19de87046c2fa9b8aa3482cd7ee52f947cb379
Anne-Kathrin <annekdrotleff@gmail.com>
2020-04-20 12:03:12 +0200

    del all
    made a mistake in using stencil.

---
+++ my-component/dist/esm-es5/my-component.mjs
@@ -0,0 +1,4 @@
+import { p as patchBrowser, b as bootstrapLazy } from './index-c8af5f01.js';
+patchBrowser().then(function (options) {
+    return bootstrapLazy([["my-component", [[1, "my-component", { "first": [1], "middle": [1], "last": [1] }]]]], options);
+});
---
+++ my-component/dist/my-component/my-component.esm.js
@@ -0,0 +1,1 @@
+import{p as a,b as m}from"./p-8baad6aa.js";a().then(a=>m([["p-cd4e6185",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],a));
