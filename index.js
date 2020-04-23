7b37bf0b4b3aff2d42fcfbe7d556b4b6b3faf8eb
Anne-Kathrin <annekdrotleff@gmail.com>
2020-04-23 20:12:55 +0200

    flip function

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const DOMNav = document.querySelector("nav");
 const DOMBackground = document.querySelector("#background");
 const DOMWrapper = document.querySelector("#wrapper");
 
+const DOMCard = document.getElementById('card');
+DOMCard.addEventListener("click", flipCard);
 
+function flipCard(){
+    DOMCard.classList.add(".card_hover");
+}
