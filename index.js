152280a41104422fe1e2f54e1ba1bded100f6a52
Anne-Kathrin <annekdrotleff@gmail.com>
2020-04-23 21:25:20 +0200

    Popup function finished

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+// POP UP
+const NAVCards = document.querySelector("#nav-index-card");
 const DOMNav = document.querySelector("nav");
 const DOMBackground = document.querySelector("#background");
 const DOMWrapper = document.querySelector("#wrapper");
+const DOMPopup = document.querySelector("#popup");
+const PopupClose = document.querySelector("#close");
 
+NAVCards.addEventListener("click", showPopUp);
+PopupClose.addEventListener("click", hidePopUp);
+
+function showPopUp(){
+    DOMNav.classList.add("blur");
+    DOMBackground.classList.add("blur");
+    DOMWrapper.classList.add("blur");
+
+    DOMPopup.style.display = "block";
+}
+
+function hidePopUp(){
+    DOMNav.classList.remove("blur");
+    DOMBackground.classList.remove("blur");
+    DOMWrapper.classList.remove("blur");
+
+    DOMPopup.style.display = "none";
+}
+
+
+
+
+
+
+
+// FLIP INDEX CARD
 const DOMCard = document.getElementById('card');
 const CardBack = document.querySelector(".card--back");
 const CardFront = document.querySelector(".card--front");
