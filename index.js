c7d57bb661b4b752f6cd3e445e3fdcfe90edc8d7
Martin <m.panaget@t-online.de>
2020-04-24 14:36:25 +0200

    First Version

--- index.js
+++ index.js
@@ -43,3 +43,49 @@ DOMCard.addEventListener('click', function() {
   CardFront.classList.remove('display');
   DOMButton.style.display = "flex";
 });
+
+
+
+
+
+// Show Images 
+
+let imagesArray = [];
+let i = 0;
+const course = document.querySelector('.course');
+const okButton = document.querySelector('.check').firstElementChild;
+const cancelButton = document.querySelector('.check').lastElementChild;
+
+okButton.addEventListener('click',showImages);
+cancelButton.addEventListener('click',showImages);
+
+for(let i = 1; i<=20; i++){
+  imagesArray[i]=i;
+}
+
+showImages();
+
+function getSrc(){
+  switch(course.innerHTML){
+      case 'ON19':
+          return 'assets/ON19/';
+      case 'ON18':
+          return 'assets/ON18/';
+      case 'ON17':
+          return 'assets/ON17/';
+      default:
+          return 'Jemand hat einen Fehler gemacht'
+  }
+}
+
+
+function showImages(){
+  i++;
+  if(i<imagesArray.length){
+    //Vorderseite
+    CardFront.firstElementChild.src = `${getSrc()}${i}-vs.jpeg`; 
+    //Rückseite
+    CardBack.firstElementChild.src = `${getSrc()}${i}-rs.jpeg`;
+  }
+}
+
