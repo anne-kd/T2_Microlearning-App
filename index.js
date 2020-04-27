6abe3cc01b98bcdabeff0fcd416a0d67eb0ab4c0
fabiangeitner <fabian-geitner@web.de>
2020-04-27 20:13:55 +0200

    new_Uebersichtseite

--- index.js
+++ index.js
@@ -18,32 +18,32 @@ PopupClose.forEach(element => {
 });
 
 
-function firstPopUp(){
-    blurElements();
-    popupFirst.style.display = "block";   
+function firstPopUp() {
+  blurElements();
+  popupFirst.style.display = "block";
 }
-function roundPopUp(p_round){
-    blurElements();
-    popupRound.style.display = "block";
-    let DOMH3 = popupRound.querySelector("#round");
-    DOMH3.innerHTML = "";
-    DOMH3.innerHTML = `Sie haben Runde ${p_round} geschafft!`;
-    
-}
+function roundPopUp(p_round) {
+  blurElements();
+  popupRound.style.display = "block";
+  let DOMH3 = popupRound.querySelector("#round");
+  DOMH3.innerHTML = "";
+  DOMH3.innerHTML = `Sie haben Runde ${p_round} geschafft!`;
 
-function hidePopUp(){
-    DOMNav.classList.remove("blur");
-    DOMBackground.classList.remove("blur");
-    DOMWrapper.classList.remove("blur");
-
-    popupFirst.style.display = "none";
-    popupRound.style.display = "none";
 }
 
-function blurElements(){
-    DOMNav.classList.add("blur");
-    DOMBackground.classList.add("blur");
-    DOMWrapper.classList.add("blur");
+function hidePopUp() {
+  DOMNav.classList.remove("blur");
+  DOMBackground.classList.remove("blur");
+  DOMWrapper.classList.remove("blur");
+
+  popupFirst.style.display = "none";
+  popupRound.style.display = "none";
+}
+
+function blurElements() {
+  DOMNav.classList.add("blur");
+  DOMBackground.classList.add("blur");
+  DOMWrapper.classList.add("blur");
 }
 
 
@@ -57,7 +57,7 @@ const CardBack = document.querySelector(".card--back");
 const CardFront = document.querySelector(".card--front");
 const DOMCheckButton = document.querySelector("#check");
 
-DOMCard.addEventListener('click', function() {
+DOMCard.addEventListener('click', function () {
   DOMCard.classList.add('card_hover');
   CardBack.classList.add('display');
   CardFront.classList.remove('display');
@@ -65,7 +65,7 @@ DOMCard.addEventListener('click', function() {
 });
 
 
-function flipCardReverse(){
+function flipCardReverse() {
   DOMCard.classList.remove('card_hover');
   CardBack.classList.remove('display');
   CardFront.classList.add('display');
@@ -77,29 +77,29 @@ function flipCardReverse(){
 // Gibt Pfad des jeweiligen Kurses zurück 
 // wird später on click auf den button 0N.. aufgerufen
 const course = document.querySelector('.course');
-function getSrc(){
-  switch(course.innerHTML){  //innerHTML ändern und klick funktion
-      case 'ON19':
-          return 'assets/ON19/';
-      case 'ON18':
-          return 'assets/ON18/';
-      case 'ON17':
-          return 'assets/ON17/';
-      default:
-          return 'Jemand hat einen Fehler gemacht'
+function getSrc() {
+  switch (course.innerHTML) {  //innerHTML ändern und klick funktion
+    case 'ON19':
+      return 'assets/ON19/';
+    case 'ON18':
+      return 'assets/ON18/';
+    case 'ON17':
+      return 'assets/ON17/';
+    default:
+      return 'Jemand hat einen Fehler gemacht'
   }
 }
 
-function getStudents(){
-  switch(course.innerHTML){
-      case 'ON19':
-          return 19;
-      case 'ON18':
-          return 30;
-      case 'ON17':
-          return 27;
-      default:
-          return 'Jemand hat einen Fehler gemacht'
+function getStudents() {
+  switch (course.innerHTML) {
+    case 'ON19':
+      return 19;
+    case 'ON18':
+      return 30;
+    case 'ON17':
+      return 27;
+    default:
+      return 'Jemand hat einen Fehler gemacht'
   }
 }
 
@@ -109,15 +109,15 @@ let count = 1;
 let inx = 0;
 let round = 1;
 let arrayRound1 = [];
-let newArray= [];
+let newArray = [];
 
 
 //Funktion erstellt Array beim ersten mal, soll bei Click auf den 
 //Kurs Button ausgelöst werden
 createArray();
-function createArray(){
+function createArray() {
   let num = getStudents();
-  
+
   for (let x = num; x > 0; x--) {
     arrayRound1.push(x);
   }
@@ -127,7 +127,7 @@ function createArray(){
 }
 
 //Hier evtl für später wenn wir unterschiedliche Arrays haben
-function getCurrentArray(){
+function getCurrentArray() {
   switch (round) {
     case 1: return arrayRound1;
     case 2: return newArray;
@@ -136,10 +136,10 @@ function getCurrentArray(){
 }
 
 //Macht jeweiliges Bild sichtbar
-function showImages(){
+function showImages() {
   let arr = getCurrentArray();
   //Vorderseite
-  CardFront.firstElementChild.src = `${getSrc()}${arr[inx]}-vs.jpeg`; 
+  CardFront.firstElementChild.src = `${getSrc()}${arr[inx]}-vs.jpeg`;
   //Rückseite
   CardBack.firstElementChild.src = `${getSrc()}${arr[inx]}-rs.jpeg`;
 }
@@ -151,57 +151,57 @@ DOMButtonFalse.addEventListener("click", countAndNew);
 DOMButtonRight.addEventListener("click", displayCardsingle);
 DOMButtonFalse.addEventListener("click", displayCardUp);
 
-function countAndNew(){
+function countAndNew() {
   flipCardReverse();
-  count ++;
+  count++;
   inx++;
 
   showImages();
   let arr = getCurrentArray();
 
   counterUp(count, arr);
-  if (count == arr.length+1){
+  if (count == arr.length + 1) {
     roundPopUp(round);
     console.log(arr);
     count = 1;
     inx = 0;
-    round ++;
+    round++;
   }
 }
 
 //COUNTER FUNKTION
-function counterUp(p_count, p_arr){
-   let arrlength = p_arr.length;
-   console.log(p_count, arrlength);
-   currentCard.innerText = "";
-   currentCard.innerText = `Karte: ${count}/${ arrlength}`;
+function counterUp(p_count, p_arr) {
+  let arrlength = p_arr.length;
+  console.log(p_count, arrlength);
+  currentCard.innerText = "";
+  currentCard.innerText = `Karte: ${count}/${arrlength}`;
 }
 
 //Funktion zum mischen
 function shuffle(p_arr) {
   let a = p_arr;
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
 
 //Nummer wird zweimal in ein neues Array eingefügt (click auf false)
-function displayCardUp(){
+function displayCardUp() {
   let arr = getCurrentArray();
-  let num = arr[inx-1];
+  let num = arr[inx - 1];
 
-    newArray.push(num, num);
-    console.log(newArray);
-  
+  newArray.push(num, num);
+  console.log(newArray);
+
 }
 
 //Nummer wird einmal in ein neues Array eingefügt
-function displayCardsingle(){
+function displayCardsingle() {
   let arr = getCurrentArray();
-  let num = arr[inx-1];
+  let num = arr[inx - 1];
 
-    newArray.push(num);
-    console.log(newArray);
+  newArray.push(num);
+  console.log(newArray);
 }
