7dc10b98ac9cd5bea57c5a2dad671b5fec61e8bd
fabiangeitner <fabian-geitner@web.de>
2020-04-30 16:48:24 +0200

    doughnut-chart

--- index.js
+++ index.js
@@ -35,22 +35,22 @@ function firstPopUp() {
 
 function roundPopUp(p_round, p_falseCounter) {
 
-    if (p_falseCounter == 0 && p_round%2 == 1) {
-      blurElements();
-      popupFirst.style.display = "block";
-      let DOMH3 = popupFirst.querySelector("#test");
-      DOMH3.innerHTML = "";
-      DOMH3.innerHTML = `Super! Sie haben Runde ${p_round} ohne einen einzigen Fehler geschafft!
+  if (p_falseCounter == 0 && p_round % 2 == 1) {
+    blurElements();
+    popupFirst.style.display = "block";
+    let DOMH3 = popupFirst.querySelector("#test");
+    DOMH3.innerHTML = "";
+    DOMH3.innerHTML = `Super! Sie haben Runde ${p_round} ohne einen einzigen Fehler geschafft!
       Wählen Sie einen neuen Kurs, den Sie lernen möchten`;
-      stopGame();
-    }
-    else {
-      popupRound.style.display = "block";
-      blurElements();
-      let DOMH3 = popupRound.querySelector("#round");
-      DOMH3.innerHTML = "";
-      DOMH3.innerHTML = `Sie haben Runde ${p_round} geschafft!`;
-    }
+    stopGame();
+  }
+  else {
+    popupRound.style.display = "block";
+    blurElements();
+    let DOMH3 = popupRound.querySelector("#round");
+    DOMH3.innerHTML = "";
+    DOMH3.innerHTML = `Sie haben Runde ${p_round} geschafft!`;
+  }
 }
 
 function hidePopUp() {
@@ -93,8 +93,8 @@ function flipCardReverse() {
 // Ändern des Kurses 
 const courseNamePopUp = document.querySelectorAll('.ON');
 
-courseNamePopUp.forEach(element=>{
-  element.addEventListener('click', (event)=>{
+courseNamePopUp.forEach(element => {
+  element.addEventListener('click', (event) => {
     setCourse(event.target);
     createArray();
     hidePopUp();
@@ -119,8 +119,8 @@ DOMButtonReset.addEventListener("click", stopGame);
 
 //verändert beim auswählen eines Kurses den Kursnamen
 const course = document.querySelector('.course');
-function setCourse(courseName){
-  switch(courseName.innerHTML){  
+function setCourse(courseName) {
+  switch (courseName.innerHTML) {
     case 'ON19':
       course.innerHTML = 'ON19';
       break;
@@ -131,14 +131,14 @@ function setCourse(courseName){
       course.innerHTML = 'ON17';
       break;
     default:
-        'Jemand hat einen Fehler gemacht'
+      'Jemand hat einen Fehler gemacht'
       break;
   }
 }
 
 function getSrc() {
   switch (
-    course.innerHTML //innerHTML ändern und klick funktion
+  course.innerHTML //innerHTML ändern und klick funktion
   ) {
     case "ON19":
       return "assets/ON19/";
@@ -207,9 +207,9 @@ function countAndNew() {
   inx++;
   showImages();
   let arr = getCurrentArray();
-    if (inx == arr.length) {
-      roundPopUp(round, falseCounter);
-    }
+  if (inx == arr.length) {
+    roundPopUp(round, falseCounter);
+  }
   flipCardReverse();
 }
 
@@ -243,12 +243,12 @@ function displayCardUp() {
   } else if (round % 2 == 1) {
     evenArray.push(num, num);
   } else if (round % 2 == 0) {
-      if (!oddArray.includes(num)) {
-        oddArray.push(num);
-      } 
-      else {
-        console.log(oddArray);
-      }
+    if (!oddArray.includes(num)) {
+      oddArray.push(num);
+    }
+    else {
+      console.log(oddArray);
+    }
   }
 }
 // Arrays werden geändert und geshuffelt
@@ -281,3 +281,31 @@ function stopGame() {
   falseCounter = 0;
   console.log(arr);
 }
+
+
+
+/* Trefferquote - Chart */
+
+var donutChart = document.getElementById('myChart');
+
+var chart = new Chart(donutChart, {
+  type: 'doughnut',
+  data: {
+    labels: ['Richtig', 'Falsch'],
+    datasets: [{
+      label: 'Trefferquote',
+      backgroundColor: ['#88bc48', 'salmon'],
+      data: [70, 30]
+    }]
+  },
+
+  options: {
+    cutoutPercentage: 65,
+    animation: {
+      animateScale: true
+    },
+    legend: {
+      display: false
+    },
+  }
+});
