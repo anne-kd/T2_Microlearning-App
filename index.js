88337e7df4a281411aec2d5c9bb71a27296e86a2
Juliane <57350721+juliane99@users.noreply.github.com>
2020-05-05 14:02:20 +0200

    hello

--- index.js
+++ index.js
@@ -14,34 +14,10 @@ const DOMWrapper = document.querySelector("#container");
 function onboardingPopUp(){
   blurElements();
   Popuponboarding.style.display = "block";
-<<<<<<< HEAD
-  DOMPopupClose.addEventListener("click", hidePopUp);
-}
-
-function roundPopUp(p_round, p_falseCounter) {
-  if (p_falseCounter == 0 && p_round % 2 == 1) {
-    blurElements();
-    popupFirst.style.display = "block";
-    let DOMH3 = popupFirst.querySelector("#test");
-    DOMH3.innerHTML = "";
-    DOMH3.innerHTML = `Super! Sie haben Runde ${p_round} ohne einen einzigen Fehler geschafft!
-    <br />  Wählen Sie einen neuen Kurs, den Sie lernen möchten`;
-    stopGame();
-  } 
-  else {
-    popupRound.style.display = "block";
-    blurElements();
-    let DOMH3 = popupRound.querySelector("#round");
-    DOMH3.innerHTML = "";
-    DOMH3.innerHTML = `Sie haben Runde ${p_round} geschafft!`;
-  }
-}
-=======
 }
 
 window.addEventListener("load", onboardingPopUp);
 
->>>>>>> eecc988eadb9b922f120b979898c75344833a05d
 //Funktion um Popups auszublenden
 function hidePopUp() {
   DOMNav.classList.remove("blur");
